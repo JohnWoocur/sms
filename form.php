6d78ba92f047575cf931d156e8b32dd4216d13dd
mathushajinithevarasa <mathushajinim@gmail.com>
2026-06-07 10:49:45 +0530

    form created

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+echo "welcome to mental hospital!"
+?>
+</br>
+</br>
+
+<form>
+    Name:<input type="text">
+</br> 
+</br>
+    City:<input type="text">
+</br>
+</br>
+    PhoneNo:<input type="text">
+</br>
+</br>
+    Email:<input type="text">
+  </br>
+  </br>  
+<input type="submit">
+</form>
